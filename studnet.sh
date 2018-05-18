cc4592fc77d88a52b16cc9570c8f24165c6b42f4
kinafu <20564946+kinafu@users.noreply.github.com>
2018-05-19 00:38:19 +0200

    edit wrong curl line may be tested with https://self-signed.badssl.com old command checked for oscp-stapling (accidentally) thx @rugk

--- studnet.sh
+++ studnet.sh
@@ -4,12 +4,12 @@ studnetPass=""
 
 HOST=https://bing.com
 while true; do
-        while ! (curl --head --silent --connect-timeout 2 --cert-status "$HOST"  > /dev/null && sleep 10); do #if 'ping' unsuccesful attempt to reconnect
-                echo "Pinging $HOST was unsucessful." >2
-                echo "Reconnecting now"
-                kill %1 > /dev/null 2>&1
-                sleep 3
+	while ! (curl --head --silent --connect-timeout 2 "$HOST"  > /dev/null && sleep 10); do #if 'ping' unsuccesful attempt to reconnect
+			echo "Pinging $HOST was unsucessful." >2
+			echo "Reconnecting now"
+			kill %1 > /dev/null 2>&1
+			sleep 3
 sshpass -p "$studnetPass" ssh -t -t -o StrictHostKeyChecking=no "$studnetNr"@139.18.143.253 &
-                sleep 2
-        done
+			sleep 2
+		done
 done
